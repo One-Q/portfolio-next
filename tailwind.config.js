b8f367aa1243ad71caac4364f6d9a66f7bf87c70
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2022-12-15 20:47:27 +0100

    chore(): eslint - tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#242743',
+        secondary: '#724CF9',
+        tertiary: '#4B7F52',
+        background: '#F8F7F9'
+      }
+    }
+  },
+  plugins: []
+};
